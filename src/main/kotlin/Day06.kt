472473a0db35eebdbc2c675696c42a3a44cb647c
Nils Druyen <info@nilsdruyen.de>
2023-12-06 22:14:49 +0100

    Solve Day06.kt

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,13 +1,42 @@
 fun main() {
-    fun part1(input: List<String>): Int = input.size
+    fun part1(input: List<String>): Int {
+        val lines = input
+            .map { line ->
+                Regex("[0-9]+").findAll(line)
+                    .map { it.value.toLong() }
+                    .toList()
+            }
 
-    fun part2(input: List<String>): Int = input.size
+        val races = List(lines.first().size) {
+            Race(lines[0][it], lines[1][it])
+        }
+
+        return races
+            .asSequence()
+            .map { it.findWins() }
+            .reduce { a, i -> a * i }
+    }
+
+    fun part2(input: List<String>): Int {
+        val lines = input.map { line -> line.filter { it.isDigit() }.toLong() }
+        return Race(lines[0], lines[1]).findWins()
+    }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day0X_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day06_test")
+    check(part2(testInput) == 71503)
 
-    val input = readInput("Day0X")
+    val input = readInput("Day06")
     part1(input).println()
     part2(input).println()
 }
+
+data class Race(val duration: Long, val distance: Long)
+fun Race.findWins(): Int = sequence { yieldAll(0..duration) }
+    .map {
+        val hold = it
+        val diff = duration - it
+        hold * diff
+    }
+    .filter { it > distance }
+    .count()
